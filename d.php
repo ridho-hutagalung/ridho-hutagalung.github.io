40720a5b17cfb7f8280d94730b8cc8e3742b8cd8
ridho <gunawan97htg@gmail.com>
2020-07-29 21:51:17 +0700

    penambahan data

---
+++ d.php
@@ -0,0 +1,14 @@
+<h2>About!</h2>
+<?php 
+echo "Tanggal hari ini : ".date("d-m-Y");
+echo " pukul : ".date("H:i:s");
+?>
+<p>
+Web IKASmansa (Ikatan Keluarga Alumni SMA Negeri Satu) ini adalah web untuk bertukar informasi antar alumni dari SMA Negeri Satu Kota..
+</p>
+<p>
+Web ini dirancang untuk dapat menginformasikan kepada anggota tentang kegiatan dari IKASmansa, dan juga sebagai tempat untuk berbagi informasi atau pun lainnya.
+</p>
+<p>
+Bagi alumni yang belum terdaftar di dalam direktori, dimohon kiranya untuk melakukan pencatatan diri dengan mengklik menu registrasi di sebelah kiri dari tampilan ini.
+</p>
